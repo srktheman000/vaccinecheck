f77509c871a8d6a1f41fe9dfd7f52b78095d22d5
sohan <sohan.kinage000@gmail.com>
2022-05-20 10:02:04 +0530

    initial commit

---
+++ src/Component/Date.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+import "./Date.css";
+
+function Date(props) {
+  const [date, setDate] = useState("08-04-2022");
+
+  const handleChange = (e) => {
+    const newdate = e.target.value.split("-").reverse();
+    const mydate = newdate.join("-");
+
+    setDate(mydate);
+  };
+  // function predate() {
+  //   let today = new Date();
+  //   let dd = today.getDate();
+  //   let mm = today.getMonth();
+  //   let yy = today.getFullYear();
+
+  //   return yy + "-" + mm + "-" + dd;
+  // }
+
+  const clickHandler = () => {
+    console.log(date);
+    props.CentresData(date);
+  };
+
+  return (
+    <div className="container">
+      <input
+        type="date"
+        min={props.mindate}
+        className="input-date"
+        onChange={handleChange}
+      />
+      <button className="btn btn-outline-success" onClick={clickHandler}>
+        Check Availability
+      </button>
+    </div>
+  );
+}
+
+export default Date;
